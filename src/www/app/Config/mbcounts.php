7381f4c459302f02d3328c6128c1d5c4d74af13e
mb counts <oreida.mbcounts@gmail.com>
2015-06-27 18:43:47 -0600

    config dir

---
+++ src/www/app/Config/mbcounts.php
@@ -0,0 +1,17 @@
+<?php
+
+$config['council_info'] =
+    array(
+        'address' => '8901 W Franklin Road',
+        'city' => 'Boise',
+        'state' => 'ID',
+        'zip' => '83704',
+    );
+
+$config['uris'] =
+    array(
+        'BSAMeritBadgeList' => 'http://www.scouting.org/meritbadges.aspx'
+    );
+
+
+// Configure::write('uris.BSAMeritBadgeList', 'http://www.scouting.org/meritbadges.aspx');
